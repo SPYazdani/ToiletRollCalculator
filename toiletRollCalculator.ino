fefecae57a3f4e511fd64177cf80c4700e0b079c
SPYazdani <96598092+SPYazdani@users.noreply.github.com>
2021-12-26 22:40:05 +1100

    Add files via upload - update to readme file

---
+++ toiletRollCalculator.ino
@@ -0,0 +1,30 @@
+int num=30;
+int sizeofpaper=5;
+int answer=0;
+int pin13=13;
+
+void setup()
+{
+  answer=num/sizeofpaper;
+  pinMode(pin13,OUTPUT);
+
+}
+
+void loop()
+{
+    switch(answer)
+  {
+    case 0:
+    digitalWrite(pin13,LOW);
+    break;
+  }
+  switch(answer)
+  {
+    case 6:
+    digitalWrite(pin13,HIGH);
+    delay (200);
+    digitalWrite(pin13,LOW);
+    delay(200);
+    break;
+  }
+}
